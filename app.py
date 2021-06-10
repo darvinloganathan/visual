ff79f9fb5e452793c7864f339dc3640c73ebc4fc
darvinloganathan <51888073+darvinloganathan@users.noreply.github.com>
2021-06-10 18:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('try3.html')
+    return render_template('try.html')
 if __name__ == "__main__":
     app.run()
     
